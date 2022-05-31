5d59eabe85abc46f214da02ece4e26ec300232ea
GlowStorm18 <105705663+GlowStorm18@users.noreply.github.com>
2022-05-31 08:23:26 +0200

    Add files via upload

--- User.py
+++ User.py
@@ -10,27 +10,28 @@ class User(Player.Player):
         return self.__points
     def AddPoints(self):
         self.__points += 1
+        #if self._pos_x and self._pos_y ==:ss
     def Move(self):
         user_input = input("Move: ")
         if user_input == 'a':
-            if self._pos_x == 0:
-                self._pos_x = 3
-            else:
-                self._pos_x -= 1
-        elif user_input == 'd':
-            if self._pos_x == 3:
-                self._pos_x = 0
-            else:
-                self._pos_x += 1
-        elif user_input == 'w':
             if self._pos_y == 0:
                 self._pos_y = 3
             else:
                 self._pos_y -= 1
-        elif user_input == 's':
+        elif user_input == 'd':
             if self._pos_y == 3:
                 self._pos_y = 0
             else:
                 self._pos_y += 1
+        elif user_input == 'w':
+            if self._pos_x == 0:
+                self._pos_x = 3
+            else:
+                self._pos_x -= 1
+        elif user_input == 's':
+            if self._pos_x == 3:
+                self._pos_x = 0
+            else:
+                self._pos_x += 1
         else:
             print("Wrong input")
